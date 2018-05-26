2cc8a1965951c574c008a58fbd5cc0fa6e93bfbe
mars <511391805@qq.com>
2018-05-27 01:38:51 +0800

    后台登录

---
+++ app/Http/Controllers/Admin/EntryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EntryController extends Controller
+{
+    public function loginForm()
+    {
+        return 'login';
+    }
+}
